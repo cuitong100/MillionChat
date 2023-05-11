84d3dc6a55f073e38c3b3e0d2e60617824931701
cuitong100 <teresa.cui@hotmail.com>
2023-05-10 21:53:36 -0700

    create handler

--- server/internal/user/user_handler.go
+++ server/internal/user/user_handler.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 type Handler struct {
 	Service
 }
+
+// Constructor of handler
 func NewHandler(s Service) *Handle{
 	return &Handler{
 		Service: s,
@@ -11,5 +13,18 @@ func NewHandler(s Service) *Handle{
 }
 
 func (h *Handler) CreatedUser(c *gin.Context) {
-	
+	var u CreatedUserReq
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error"; err.Error})
+		return
+	}
+
+	res, err := h.Service.CreatedUser(c.Request.Context(), &u)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error" : err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, res)
+
+
 }
